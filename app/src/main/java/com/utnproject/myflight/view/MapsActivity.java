6a0833247ecb5aa987cb882c00c402251bffddf7
Jose Villalobos <lvilllaobos@gmail.com>
2018-11-21 03:23:10 -0600

    airplane icon to maps

--- app/src/main/java/com/utnproject/myflight/view/MapsActivity.java
+++ app/src/main/java/com/utnproject/myflight/view/MapsActivity.java
@@ -42,8 +42,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        MarkerOptions marker = new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.icon));
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        MarkerOptions marker = new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_plane));
+        mMap.addMarker(marker.position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
