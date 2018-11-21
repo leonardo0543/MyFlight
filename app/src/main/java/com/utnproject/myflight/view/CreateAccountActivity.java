c52f08d03e095dddd2707b397a3f36475104d3a0
Jose Villalobos <lvilllaobos@gmail.com>
2018-11-21 00:40:28 -0600

    toolbar creation

--- app/src/main/java/com/utnproject/myflight/view/CreateAccountActivity.java
+++ app/src/main/java/com/utnproject/myflight/view/CreateAccountActivity.java
@@ -1,7 +1,9 @@
 package com.utnproject.myflight.view;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.utnproject.myflight.R;
 
@@ -11,5 +13,12 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolbar(getResources().getString(R.string.toolbar_tittle_create_account),false);
+    }
+
+    @SuppressLint("RestrictedApi")
+    public void showToolbar(String tittle, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(upButton);
     }
 }
